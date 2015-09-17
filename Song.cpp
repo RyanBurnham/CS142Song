c33174fe7a9019886e9032c549f706cf17fd1d01
RyanBurnham <burnhamrw@gmail.com>
2015-09-16 22:46:37 -0400

    Struct Song to the Class Song
    Made the Song structure into a class. Made the members artist and title into private members, and moved the functions: Initialize, print and is_equal into the class as private methods. Added the method initialize taking initial arguments. The parameters of the functions were updated to proper method parameters. Added a test function as the main function.

--- Song.cpp
+++ Song.cpp
@@ -1,22 +1,85 @@
-struct
+#include <string>
+using std::string;
+
+#include <iostram>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::istream;
+using std::ostream;
+
+class Song
 {
-  string artist;
-  string title;
+public:
+  inline void initialize();
+  inline void initialize(const string & title, const string & artist);
+  
+  inline void print(ostream & out) const;
+  inline bool is_equal(const Song & other) const;
+  
+private:
+  string artist_;
+  string title_;
 };
 
-void initialize(Song & song)
+inline void Song::initialize()
 {
-  song.artist = "none";
-  song.title = "invalid";
+  artist_ = "none";
+  title_ = "invalid";
 }
 
-void print(const Song & song, ostream & out)
+inline void Song::initialize(string & title, string & artist)
 {
-  out << song.title << endl << song.artist;
+  title_ = title;
+  artist_ = artist;
 }
 
-bool is_equal(const Song & song1, const Song & song2)
+inline void Song::print(ostream & out) const
 {
-  return song1.title == song2.title && song1.artist == song2.artist;
+  out << song.title_ << endl << song.artist_;
+}
+
+inline bool Song::is_equal(const Song & song2) const
+{
+  return title_ == song2.title && artist_ == song2.artist;
+}
+
+int main()
+{
+  while(true)
+  {
+    string t_artist;
+    string t_title;
+    
+    cout << "Enter a song name: ";
+    getline(cin, t_title);
+    cout << "Enter the artist of the song: ";
+    getline(cin, t_artist);
+    
+    Song song1;
+    song1.initialize(t_title, t_artist);
+    
+    cout << "Enter a song name: ";
+    getline(cin, t_title);
+    cout << "Enter the artist of the song: ";
+    getline(cin, t_artist);
+    
+    Song song2;
+    song2.initialize(t_title, t_artist);
+    
+    if(song1.is_equal(song2))
+      cout << "They are the same song." << endl;
+    else
+      cout << "They are different songs." << endl;
+      
+    cout << "The first song is:\n";
+    song1.print(cout);
+    cout << "The second song is:\n";
+    song2.print(cout);
+    
+    cout << "RESTARTING TEST\n";
+  }
+  
+  return 0;
 }
 
