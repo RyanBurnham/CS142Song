8d722e07ca92886993e94649573f2254b2348e3e
Ryan Burnham <burnharw@clarkson.edu>
2015-09-18 14:21:11 -0400

    Added the operator = which copies the title and artist to the left element. Also made the operator << return the ostream to allow chains of outputs.

--- Song.cpp
+++ Song.cpp
@@ -16,6 +16,8 @@ public:
 
   inline bool is_equal(const Song & other) const;
 
+  inline void operator=(const Song & value);
+
   string artist() const {return artist_;}
   string title() const {return title_; }
 
@@ -24,7 +26,13 @@ private:
   string title_;
 };
 
-inline void operator<<(ostream & out, const Song & song)
+inline void Song::operator=(const Song & value)
+{
+    artist_ = value.artist_;
+    title_ = value.title_;
+}
+
+inline ostream & operator<<(ostream & out, const Song & song)
 {
   out << song.title() << endl << song.artist() << endl;
 }
@@ -64,6 +72,17 @@ int main()
     cout <<song1;
     cout << "The second song is:\n";
     cout << song2;
+
+    cout << "Copying song1 to song 2" << endl;
+    song1 = song2;
+
+    if(song1.is_equal(song2))
+      cout << "They are the same song." << endl;
+    else
+      cout << "They are different songs." << endl;
+
+    cout << "Song1: " << song1;
+    cout << "Song2: " << song2;
 
     cout << "RESTARTING TEST\n";
   }
